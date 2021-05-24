5f898a70d38aaa229cbde11ca0ebbc348453549c
Austin Yeon <austinyeon@gmail.com>
2021-05-24 10:53:10 -0700

    can add X to first square on board

--- app.js
+++ app.js
@@ -3,8 +3,16 @@ console.log('hello from app!');
 // Event listener on each of the tables to identify when clicked
 
 const addXO = () => {
-
+  let square = document.getElementById('s1');
+  if (square.firstChild.nodeValue === '') {
+    square.firstChild.nodeValue = 'X';
+  } else {
+    square.firstChild.nodeValue = '';
+  }
 }
 
+// add event listener to the table
+const el = document.getElementById('board');
+el.addEventListener('click', addXO, false);
 // Character generator that produces a X or O for the table to fill in when clicked
 
