ae5ebe15e7c0b9a2731cd4aa40390cc091e97359
Austin Yeon <austinyeon@gmail.com>
2021-05-24 10:36:43 -0700

    added styling and ID to each square in table

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
 console.log('hello from app!');
+
+// Event listener on each of the tables to identify when clicked
+
+const addXO = () => {
+
+}
+
+// Character generator that produces a X or O for the table to fill in when clicked
+
